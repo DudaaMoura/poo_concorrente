2b274fbedb0bec66e57a667cd319bbbc9245a313
DudaaMoura <mariamourapalmital@gmail.com>
2023-10-31 13:49:58 +0000

    salvo

---
+++ Exemplo.java
@@ -0,0 +1,10 @@
+public class Exemplo extends Thread{
+     public void run(){
+        Processo proc1 = new Processo("Leo",17);
+        Processo proc2 = new Processo("Yara", 17);
+        Processo proc2 = new Processo("Yara", 17);
+
+
+       
+     }
+}
